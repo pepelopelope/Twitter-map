20339c86f35bbc570363305f1516f97d8c7b6afb
pepelopelope <lopezlopezjuradoelromeral@gmail.com>
2023-03-20 17:16:19 +0100

    Changes to be committed: 	modified:   stream_to_kinesis.py

--- stream_to_kinesis.py
+++ stream_to_kinesis.py
@@ -12,9 +12,12 @@ access_token_secret = 'TU_ACCESS_TOKEN_SECRET'
 # Palabra clave o hashtag para seguir
 hashtag = "#TUCERTAINHASHTAG"
 
-# Variables de configuración de Kinesis
-kinesis_client = boto3.client('kinesis', region_name='TU_REGION')
-kinesis_stream_name = 'TU_NOMBRE_DE_STREAM'
+# Variables de autenticación de Kinesis
+region_name = 'TU_REGION_NAME'
+stream_name = 'TU_NOMBRE_DE_STREAM'
+
+# Crea una instancia de Kinesis
+kinesis = boto3.client('kinesis', region_name=region_name)
 
 # Clase StreamListener
 class StreamListener(tweepy.StreamListener):
@@ -34,4 +37,25 @@ class StreamListener(tweepy.StreamListener):
                 tweet_dict['created_at'] = datetime.strptime(tweet['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
                 tweet_dict['coordinates'] = tweet['coordinates']['coordinates'] if tweet['coordinates'] else None
                 tweet_dict['user'] = {
-                    'screen_name':
+                    'screen_name': tweet['user']['screen_name'],
+                    'location': tweet['user']['location']
+                }
+                # Convierte el diccionario Python en una cadena JSON y la envía a Kinesis
+                tweet_json = json.dumps(tweet_dict)
+                response = kinesis.put_record(StreamName=stream_name, Data=tweet_json, PartitionKey='default')
+                print(response)
+        except Exception as e:
+            print(e)
+
+# Autenticación de Twitter
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# Crear una instancia de StreamListener
+listener = StreamListener()
+
+# Crear un objeto Stream de Tweepy
+stream = tweepy.Stream(auth=auth, listener=listener, tweet_mode='extended')
+
+# Iniciar el streaming de tweets que contienen el hashtag
+stream.filter(track=[hashtag])
